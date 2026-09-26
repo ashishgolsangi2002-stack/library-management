5fe2215f2555eb96b7cba47f81ed1834abaadd82
ASHISH <ASHISH@Ak>
2026-09-26 10:00:38 +0530

    Genre Controller Updated

--- src/main/java/com/library/controller/GenreController.java
+++ src/main/java/com/library/controller/GenreController.java
@@ -3,6 +3,7 @@ package com.library.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.library.modal.Genre;
@@ -12,11 +13,12 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/genres")
 public class GenreController {
 
 	private final GenreService genreService;
 	
-	@PostMapping
+	@PostMapping("/create")
 	public ResponseEntity<Genre> addGenre(@RequestBody Genre  genre){
 		Genre createdGenre= genreService.createGenre(genre);
 		return ResponseEntity.ok(createdGenre);
